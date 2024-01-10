7534be9daada2ea515f70593955e892e0f8276d7
lucas <lcsbonilla@gmail.com>
2024-01-10 14:03:37 -0300

    chore(db): modifica arquivo init.db com a estrutura das tabelas

--- init.sql
+++ init.sql
@@ -1,1 +1,14 @@
 CREATE SCHEMA IF NOT EXISTS freterapidoapi;
+
+-- Tabela 'carrier'
+CREATE TABLE freterapidoapi.carrier (
+    id_carrier INTEGER PRIMARY KEY,
+    carrier_name VARCHAR(255) NOT NULL
+);
+
+-- Tabela 'quote'
+CREATE TABLE freterapidoapi.quote (
+    id_quote SERIAL PRIMARY KEY,
+    id_carrier INTEGER REFERENCES freterapidoapi.carrier(id_carrier),
+    price_quote FLOAT NOT NULL
+);
